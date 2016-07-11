70783e8a32cbdea0744536e3dc711eb5e03bdb4a
ezaman <ehsanmzaman@gmail.com>
2016-07-11 14:09:07 -0400

    interpolating strings from different arrays

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -11,15 +11,25 @@ import UIKit
 class ViewController: UIViewController {
     
     var shoppingList: [String] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let items = ["Bananas", "Apples", "Eggs", "Rolls"]
+        let numberOfItems = [6,4,12,4]
+        shoppingList = makeShoppingList(items, quantityOfItems: numberOfItems)
         
-    
-        
+        // print(shoppingList)
         
     }
     
-    
-    
+    func makeShoppingList (items:[String], quantityOfItems:[Int])->[String]{
+        var newArray: [String] = []
+        for (index, item) in items.enumerate() {
+            
+            newArray.append("\(index + 1). \(quantityOfItems[index]) \(item)")
+            
+        }
+        return newArray
+    }
 }
+
